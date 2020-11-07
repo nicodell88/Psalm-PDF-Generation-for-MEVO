3fd19999fa40d280dfa6bfab5e5d061b26ac4f4c
Nic O'Dell <nicodell88@gmail.com>
2020-11-08 00:01:02 +1100

    psalm pdf generation works

--- runPsalmAuto.py
+++ runPsalmAuto.py
@@ -4,34 +4,87 @@ import pypdftk
 import glob
 import pdftotext
 import os
+from PyPDF2 import PdfFileWriter, PdfFileReader
+import json
+import shutil
 
-book = "16x9_b_w_underlined/Sing Psalms"
-psalm = 8
-verse = [1,6]
-strr = "{book_}/*{ps_:03d}*.pdf".format(book_ = book,ps_ = psalm)
+savePath = 'LordsDay'
+if not os.path.exists(savePath):
+    os.makedirs(savePath)
 
-files = glob.glob(strr)
+tmpDir = 'tmp'
+if not os.path.exists(tmpDir):
+    os.makedirs(tmpDir)
 
-assert len(files) == 1
+f = open('OOS.json')
+# df = f.read()
+# print(x)
+df = json.loads(f.read())
 
-# gen = grep.do_grep(files[0],'[a]',ignore_case = True)
-# outputs = [output for output in gen]
-# print(outputs)
+## Read in and print sermon title etc
+print(df["Sermon"])
+print(df["Passage"])
+print(df["Date"])
+#TODO Automate Youtube thumbnail
+if os.path.exists(savePath + "/" + df["Sermon"]):
+    shutil.rmtree(savePath + "/" + df["Sermon"])
+os.makedirs(savePath + "/" + df["Sermon"])
 
-# Load your PDF
-with open(files[0], "rb") as f:
-    pdf = pdftotext.PDF(f)
-
-# How many pages?
-print(len(pdf))
-
-for j in range(2):
-    # Iterate over all the pages
-    voi = "{ver:d}".format(ver = verse[j])
-    for i in range(len(pdf)):
+def findPageWithString(pdf,voi):
+    for i in range(1,len(pdf)):
         page = pdf[i]
         # print(page)
         if voi in page:
-            print(f"verse {verse[j]:d} is on page {i+1:d}")
-            # print(page)
-            break
+            return i
+
+
+
+
+nPsalms = len(df["Psalms"])
+pdf_writer = PdfFileWriter()
+
+for i in range(nPsalms):
+    psalm = df["Psalms"][i]
+    fileSpec = "Psalms/" + psalm["book"] + "/*{ps_:03d}*{ver_}*.pdf".format(ps_ = psalm["number"],ver_ = psalm["version"])
+    # print(fileSpec)
+
+    files = glob.glob(fileSpec)
+    # print(files)
+    assert len(files) == 1
+    pdfFile = files[0]
+    # print(pdfFile)
+    with open(files[0], "rb") as f:
+        pdf = pdftotext.PDF(f)
+    # print(pdf[1])
+    # continue
+    verses = psalm["verses"].split(',')
+    PageVec = [0]
+    for j in range(len(verses)):
+        if '-' in verses[j]:
+            verRan = verses[j].split('-')
+            assert(len(verRan))==2
+            lv = verRan[0]
+            uv = verRan[1]
+            lp = findPageWithString(pdf,lv)
+            up = findPageWithString(pdf,uv)
+            PageVec += list(range(lp,up+1))
+        else:
+            ver = verses[j]
+            pp = findPageWithString(pdf,ver)
+            PageVec += list(range(pp,pp+1))
+
+    # print(PageVec)
+    # pdf_writer = PdfFileWriter()
+    with open(pdfFile, "rb") as f:
+        pdf_read = PdfFileReader(f)
+
+        for pagenum in PageVec:
+            pdf_writer.addPage(pdf_read.getPage(pagenum))
+        # outFile = "tmp/out_{pnum:d}.pdf".format(pnum = i)
+        outFile = savePath + "/" + df["Sermon"] + "/Psalms.pdf"
+        with open(outFile,'wb') as out:
+            pdf_writer.write(out) 
+    
+print("Finished")
+print("YouTube Title")
+print("{} - {} - {}".format(df["Date"],df["Passage"],df["Sermon"]))
