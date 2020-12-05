67a55c9d49d284ccacaf696b32da2c2019e14a02
Nic O'Dell <nicodell88@gmail.com>
2020-12-05 21:36:53 +1100

    Detecting the page a verse ends on

--- runPsalmAuto.py
+++ runPsalmAuto.py
@@ -7,6 +7,7 @@ import os
 from PyPDF2 import PdfFileWriter, PdfFileReader
 import json
 import shutil
+import easygui
 
 from PIL import Image
 from PIL import ImageFont
@@ -20,7 +21,10 @@ tmpDir = 'tmp'
 if not os.path.exists(tmpDir):
     os.makedirs(tmpDir)
 
-f = open('OOS.json')
+
+fileJson = easygui.fileopenbox()
+
+f = open(fileJson)
 # df = f.read()
 # print(x)
 df = json.loads(f.read())
@@ -34,13 +38,28 @@ if os.path.exists(savePath + "/" + df["Sermon"]):
     shutil.rmtree(savePath + "/" + df["Sermon"])
 os.makedirs(savePath + "/" + df["Sermon"])
 
-def findPageWithString(pdf,voi):
+def findPageWithString_start(pdf,voi):
     for i in range(1,len(pdf)):
         page = pdf[i]
         # print(page)
         if voi in page:
             return i
 
+def findPageWithString_end(pdf,voi):
+    for i in range(1,len(pdf)):
+        page = pdf[i]
+        # print(page)
+        if voi in page:
+            #Determine if verse ends on page it starts on
+            #last page
+            if i+1 not in range(1,len(pdf)):
+                return i
+                #verse finishes on page
+            if any(map(str.isdigit,pdf[i+1])):
+                return i
+                #verse continies over page
+            else:
+                return i+1
 
 
 
@@ -69,13 +88,16 @@ for i in range(nPsalms):
             assert(len(verRan))==2
             lv = verRan[0]
             uv = verRan[1]
-            lp = findPageWithString(pdf,lv)
-            up = findPageWithString(pdf,uv)
+            lp = findPageWithString_start(pdf,lv)
+            up = findPageWithString_end(pdf,uv)
             PageVec += list(range(lp,up+1))
         else:
             ver = verses[j]
-            pp = findPageWithString(pdf,ver)
-            PageVec += list(range(pp,pp+1))
+            lp = findPageWithString_start(pdf,ver)
+            up = findPageWithString_end(pdf,ver)
+            # PageVec += list(range(pp,pp+1))
+            PageVec += list(range(lp,up+1))
+
 
     # print(PageVec)
     # pdf_writer = PdfFileWriter()
@@ -93,6 +115,7 @@ print("Finished")
 print("YouTube Title")
 print("{} - {} - {}".format(df["Date"],df["Passage"],df["Sermon"]))
 
+
 font = ImageFont.truetype("Seravek.ttc",size = 65)
 img = Image.open('wmark_text_drawn.jpg')
 draw = ImageDraw.Draw(img)
@@ -108,4 +131,5 @@ w,h = draw.textsize(msg,font = font)
 draw.text(((1920-w)/2,400),msg,(255,255,255),font = font)
 
 # draw.text((1920/2,500),df["Passage"],(255,255,255),font = font,align="right")
-img.save(savePath + "/" + df["Sermon"] +  "/YT.jpg")
+ytTitle = "{} - {}".format(df["Passage"],df["Sermon"])
+img.save(savePath + "/" + df["Sermon"] +  "/" +ytTitle +".jpg")
